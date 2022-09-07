c752a11847b110896840170c816cb186f3210acd
Rocko <codingwithrock@gmail.com>
2022-09-07 21:05:54 +0600

    fixed error

--- confirm-location.js
+++ confirm-location.js
@@ -1,13 +1,13 @@
-value = confirm("Do you want to see your Location?");
+condition = confirm("Do you want to see your Location?");
 
 const checkUrl = ()=>{
-    url = location.href();
+    url = location.href;
     return url;
 }
 
-if(value)
+if(condition)
 {
-    console.log(checkUrl);
+    console.log(checkUrl());
 
 }
 
