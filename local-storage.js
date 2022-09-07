9794503e0093f1a42ac19b1f5a2bbb48bce762c6
Rocko <codingwithrock@gmail.com>
2022-09-08 01:28:59 +0600

    basic structure ready for storing values in browser local storage

---
+++ local-storage.js
@@ -0,0 +1,13 @@
+const setItem = ()=> {
+    localStorage.setItem('key','value');
+}
+
+
+const modifyValue = ()=> {
+    localStorage.setItem('key','key value Has been changed');
+}
+
+
+const clearAll = ()=>{
+    localStorage.clear();
+}
