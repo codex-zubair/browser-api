ae6d574b943055e6ce617096c5227a59497a8afa
Rocko <codingwithrock@gmail.com>
2022-09-07 21:14:14 +0600

    assing the url using assing and added the function

---
+++ change-location.js
@@ -0,0 +1,7 @@
+const changeLocation = ()=> {
+
+    window.location.assign('https://www.google.com/');
+
+    //! herf will work too.
+    // window.location.href = 'https://www.google.com/';
+}
--- confirm-location.js
+++ confirm-location.js
@@ -1,15 +1,15 @@
-condition = confirm("Do you want to see your Location?");
+// condition = confirm("Do you want to see your Location?");
 
-const checkUrl = ()=>{
-    url = location.href;
-    return url;
-}
+// const checkUrl = ()=>{
+//     url = location.href;
+//     return url;
+// }
 
-if(condition)
-{
-    console.log(checkUrl());
+// if(condition)
+// {
+//     console.log(checkUrl());
 
-}
+// }
 
 
 
